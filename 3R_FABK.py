9047da86e2306fd9b84f61cf5f655fa5642f754e
Tevin <deskcomp10@outlook.com>
2021-11-06 10:41:25 -0400

    Debugging problems with servo writing in IK

--- 3R_FABK.py
+++ 3R_FABK.py
@@ -128,9 +128,9 @@ class Kinematics: #Kinematics class
 	def play_inverse(self, x_g, y_g, n): #Params: 1, 2. The x and y coordinate to place the end effector/ the goal position's x and y coordinate
 									  #        3. Number of iterations of the algorithm we should go through 
 		# #Declare and/or Initialize essential variables
-		servo0.currentServoPos = 0 #Joint 1 (Innermost servo) is set to 0 degrees
-		servo1.currentServoPos = 180
-		servo2.currentServoPos = 180
+		servo0.set_position(0) #Joint 1 (Innermost servo) is set to 0 degrees
+		servo1.set_position(180)
+		servo2.set_position(180)
 
 		k = 0 #Joint counter
 
@@ -156,23 +156,22 @@ class Kinematics: #Kinematics class
 			mag_v_JG = np.sqrt(v_JG.dot(v_JG))
 
 			#Use the dot product formula to calculate the angle between the two vectors
-			ang = m.acos( (v_JE.dot(v_JG)) / (mag_v_JE*mag_v_JG) )
+			ang = m.acos( (v_JE.dot(v_JG)) / (mag_v_JE*mag_v_JG) ); print((ang*m.pi)/180)
 
 			#Change the kth joint's angle by the calculated angle
 			if k == 0: servo0.currentServoPos = servo0.currentServoPos + ang; print(servo0.currentServoPos)
 			elif k == 1: servo1.currentServoPos = servo1.currentServoPos + ang; print(servo1.currentServoPos)
 			elif k == 2: servo2.currentServoPos = servo2.currentServoPos + ang; print(servo2.currentServoPos)
 
-			print((ang*m.pi)/180) #Turn radians to degrees
-			print(k)
+			# print((ang*m.pi)/180) #Turn radians to degrees
+			# print(k)
 
 			sleep(10)
 
 			#Increment the joint counter
-			if k < 3: 
-					k+=1
-			elif k == 2:
-    				k = 0
+			if k < 3: k+=1
+			elif k == 2: k = 0
+			print(k-1)
 
 
 			
